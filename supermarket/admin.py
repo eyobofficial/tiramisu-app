051a4b62c7a8505cd1f4ef62793395be75d32e52
eyob tariku <eyobtariku@gmail.com>
2018-02-25 01:54:51 +0300

    Register models to admin.py & create superuser

--- supermarket/admin.py
+++ supermarket/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import Models
+from .models import Catagory, Brand, Tag, Product
+
+
+# Register Catagory, Brand & Tag Models
+admin.site.register(Catagory)
+admin.site.register(Brand)
+admin.site.register(Tag)
+
+
+# Register Product Model
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'catagory',
+        'brand',
+        'on_sale',
+        'created_at',
+        'updated_at'
+    )
