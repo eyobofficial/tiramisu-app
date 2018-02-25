a8bc50fa19f66e24a71c58ca0396cbe102d98c47
eyob tariku <eyobtariku@gmail.com>
2018-02-25 23:18:30 +0300

    Create placeholder views for Catagory & Product model

--- supermarket/views.py
+++ supermarket/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.generic.views import DetailView, ListView, TemplateView
 
-# Create your views here.
+# Import Mddels
+from .models import (
+    Catagory,
+    Product,
+)
+
+
+class IndexView(TemplateView):
+    """
+    View for Index Page
+    """
+    pass
+
+
+class CatagoryList(ListView):
+    """
+    View for listing all Product Catagory
+    """
+    model = Catagory
+
+
+class CatagoryDetail(DetailView):
+    """
+    View for a particular Product Catagory Detail
+    """
+    model = Catagory
+
+
+class ProductList(ListView):
+    """
+    View for listing all Products
+    """
+    model = Product
+
+
+class ProductDetail(DetailView):
+    """
+    View for a particular Product Detail
+    """
+    model = Product
